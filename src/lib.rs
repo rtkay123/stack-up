e4077f4753205b2771e3f0c10470aad58fba3c18
rtkay123 <dev@kanjala.com>
2025-03-08 19:21:36 +0200

    feat: tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "tracing")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tracing")))]
+pub mod tracing;
---
+++ src/tracing.rs
@@ -0,0 +1,77 @@
+use tracing_subscriber::{
+    EnvFilter, Layer, Registry, layer::SubscriberExt, util::SubscriberInitExt,
+};
+
+/// Telemetry handle
+#[allow(missing_debug_implementations)]
+pub struct Tracing {}
+
+impl Tracing {
+    /// Create a new [builder](TracingBuilder)
+    /// # Examples
+    /// ```
+    /// # use sellershut_services::tracing::Tracing;
+    /// let _tracing = Tracing::builder();
+    /// ```
+    pub fn builder() -> TracingBuilder {
+        TracingBuilder::default()
+    }
+}
+
+/// A builder for initialising [tracing] layers
+#[allow(missing_debug_implementations)]
+pub struct TracingBuilder {
+    layer: Vec<Box<dyn Layer<Registry> + Sync + Send>>,
+}
+
+impl Default for TracingBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl TracingBuilder {
+    /// Create a new builder
+    /// # Examples
+    /// ```
+    /// # use sellershut_services::tracing::TracingBuilder;
+    /// let _tracing = TracingBuilder::new();
+    /// ```
+    pub fn new() -> Self {
+        let types: Box<dyn Layer<Registry> + Sync + Send> =
+            tracing_subscriber::fmt::layer().boxed();
+        TracingBuilder { layer: vec![types] }
+    }
+
+    /// Initialises tracing with the provided level
+    /// # Examples
+    /// ```
+    /// # use sellershut_services::tracing::TracingBuilder;
+    /// let _tracing = TracingBuilder::new().build(None);
+    /// ```
+    pub fn build(self, level: Option<std::sync::Arc<str>>) -> Tracing {
+        let level = if let Some(level) = level {
+            level.to_string()
+        } else {
+            "info".to_string()
+        };
+        tracing_subscriber::registry()
+            .with(self.layer)
+            .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| level.into()))
+            .try_init()
+            .ok();
+        Tracing {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build() {
+        let builder = Tracing::builder();
+        let level = crate::config::default_log();
+        builder.build(level);
+    }
+}
