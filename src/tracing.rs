7da69ff3e05e42674716c09d624737e746a66390
rtkay123 <dev@kanjala.com>
2025-06-28 11:48:22 +0200

    feat(tracing): consolidate build

--- src/tracing.rs
+++ src/tracing.rs
@@ -13,6 +13,7 @@ use tracing_subscriber::{
 
 /// Telemetry handle
 #[derive(bon::Builder)]
+#[builder(finish_fn(vis = "", name = build_internal))]
 pub struct Tracing {
     #[builder(field = vec![tracing_subscriber::fmt::layer().boxed()])]
     layers: Vec<Box<dyn Layer<Registry> + Sync + Send>>,
@@ -24,9 +25,15 @@ pub struct Tracing {
     pub otel_provider: opentelemetry_sdk::trace::SdkTracerProvider,
 }
 
-impl Tracing {
-    pub fn initialise(&mut self, config: &crate::Monitoring) {
-        let layers = std::mem::take(&mut self.layers);
+// Define a custom finishing function as a method on the `UserBuilder`.
+// The builder's state must implement the `IsComplete` trait.
+// See details about it in the tip below this example.
+impl<S: tracing_builder::IsComplete> TracingBuilder<S> {
+    pub fn build(self, config: &crate::Monitoring) -> Tracing {
+        // Delegate to `build_internal()` to get the instance of user.
+        let mut tracing = self.build_internal();
+
+        let layers = std::mem::take(&mut tracing.layers);
         tracing_subscriber::registry()
             .with(layers)
             .with(
@@ -35,6 +42,7 @@ impl Tracing {
             )
             .try_init()
             .ok();
+        tracing
     }
 }
 
